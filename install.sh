313983c203dcbbac9a39d929e1b9eaa9b36affa6
taishin <root@test2.(none)>
2014-04-06 22:45:37 +0900

    refactor

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ if [ "$https_proxy" != "" ]; then
 fi
 
 curl -L https://www.opscode.com/chef/install.sh | bash
+git clone https://github.com/taishin/zabbix.git
 /usr/bin/chef-solo -j ./chef.json -c ./solo.rb
